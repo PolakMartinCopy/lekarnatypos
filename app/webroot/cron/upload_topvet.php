b840e0887afad6d51b89472296f73ecea70c9c83
Martin Polak <brko11@gmail.com>
2015-04-29 16:51:27 +0200

    cron pro natahovani topvetu

---
+++ app/webroot/cron/upload_topvet.php
@@ -0,0 +1,18 @@
+<?php 
+$url = 'http://www.lekarnatypos.cz/suppliers/upload/1';
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+$st = curl_exec($ch);
+curl_close($ch);
+if (!empty($st)) {
+	$headers .= "Content-Type: text/plain; charset = \"UTF-8\";\n";
+	$headers .= "Content-Transfer-Encoding: 8bit\n";
+	$headers .= "\n";
+	
+	mail('brko11@gmail.com', 'LekarnaTypos CZ - problem pri natahovani Topvetu', $st, $headers);
+//	mail('vlado.tovarnak@gmail.com', 'Možný problém na OE', $st, $headers);
+}
+?>
