aa7950b2c9d1909f5f2d8f796cefabc51b64c451
Martin Polak <brko11@gmail.com>
2015-10-12 15:24:27 +0200

    optimalizace fce pro zjisteni, zda updatovat property daty z feedu

--- app/models/product.php
+++ app/models/product.php
@@ -906,21 +906,19 @@ class Product extends AppModel {
 	}
 	
 	function is_product_property_editable($id, $product_property_id, $supplier_id) {
-		// u produktu syncare nechci updatovat nic
-		$product_property = $this->ProductPropertiesProduct->find('first', array(
-			'conditions' => array(
-				'ProductPropertiesProduct.product_id' => $id,
-				'ProductPropertiesProduct.product_property_id' => $product_property_id
-			),
-			'contain' => array(),
-		));
 		// defaultne chci updatovat
 		$update = true;
-		if (!empty($product_property) && isset($product_property['ProductPropertiesProduct']['update'])) {
-			$update = $product_property['ProductPropertiesProduct']['update'];
-			// pokud neni zadano jinak, u produktu syncare nechci updatovat
-		} elseif ($supplier_id == 2 || $supplier_id == 1 || $supplier_id == 3 || $supplier_id == 4 || $supplier_id == 5) {
+		if ($supplier_id == 2 || $supplier_id == 1 || $supplier_id == 3 || $supplier_id == 4 || $supplier_id == 5) {
 			$update = false;
+		} else {
+			$product_property = $this->ProductPropertiesProduct->find('first', array(
+				'conditions' => array(
+					'ProductPropertiesProduct.product_id' => $id,
+					'ProductPropertiesProduct.product_property_id' => $product_property_id
+				),
+				'contain' => array(),
+			));
+			$update = $product_property['ProductPropertiesProduct']['update'];
 		}
 		return $update;
 	}
