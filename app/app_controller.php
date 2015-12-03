887293d49eac07def36768e2e6eff5c9d46ca8c5
Martin Polak <brko11@gmail.com>
2015-12-03 15:53:55 +0100

    HP - oprava zobrazeni kategorii pri vlozeni utm tagu do URL

--- app/app_controller.php
+++ app/app_controller.php
@@ -138,7 +138,7 @@
 				$categories_menu = $this->Product->CategoriesProduct->Category->getSidebarMenu($opened_category_id, $this->Session->check('Customer'), false, false, true, $this->Product->CategoriesProduct->Category->category_subtree_root_id);
 				$bothers_menu = $this->Product->CategoriesProduct->Category->getSidebarMenu($opened_category_id, $this->Session->check('Customer'), false, false, true, $this->Product->CategoriesProduct->Category->bothers_subtree_root_id);
 			} elseif ($this->layout == REDESIGN_PATH . 'homepage') {
-				if ($_SERVER['REQUEST_URI'] == '/') {
+				if ($this->params['controller'] == 'pages' && $this->params['action'] == 'home') {
 					// uplny strom kategorii - vypisuje se pouze na HP
 					$categories_menu = $this->Product->CategoriesProduct->Category->getSidebarMenu($opened_category_id, $this->Session->check('Customer'), false, false, false, $this->Product->CategoriesProduct->Category->category_subtree_root_id);
 					$bothers_menu = $this->Product->CategoriesProduct->Category->getSidebarMenu($opened_category_id, $this->Session->check('Customer'), false, false, false, $this->Product->CategoriesProduct->Category->bothers_subtree_root_id);
