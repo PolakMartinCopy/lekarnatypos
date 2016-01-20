bc43895fb5fd95c1731fd34e9ba2d70d194c6997
Martin Polak <brko11@gmail.com>
2016-01-20 15:44:24 +0100

    sledovac - navstevy kategorii - fix

--- app/controllers/t_s_visit_categories_controller.php
+++ app/controllers/t_s_visit_categories_controller.php
@@ -2,10 +2,10 @@
 class TSVisitCategoriesController extends AppController {
 	var $name = 'TSVisitCategories';
 	
-	function my_create($id) {
+	function my_create($category_id) {
 		$res = array('success' => false);
 		$key = $this->TSVisitCategory->TSVisit->TSCustomerDevice->getKey($this->Cookie, $this->Session);
-		$myCreateRes = $this->TSVisitCategory->myCreate($id);
+		$myCreateRes = $this->TSVisitCategory->myCreate($category_id);
 		if ($myCreateRes) {
 			$res = array(
 				'success' => true,
