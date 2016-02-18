8566cf618aec0716463ff3d748abbf2a9e3ef9ec
Martin Polak <brko11@gmail.com>
2016-02-18 15:25:17 +0100

    google merchant feed generator - import naparovani

--- app/controllers/categories_comparators_controller.php
+++ app/controllers/categories_comparators_controller.php
@@ -51,4 +51,52 @@ class CategoriesComparatorsController extends AppController {
 		}
 		die('OK');
 	}
+	
+	function admin_init_gmc() {
+		$comparator_id = 3;
+		
+		$source_file = 'files/shop_2_taxonomy.csv';
+		$content = file_get_contents($source_file);
+		$content = explode("\n", $content);
+		$save = array();
+		foreach ($content as $line) {
+			$line = trim($line);
+			$line = str_getcsv($line, ';');
+			$category_id = false;
+			if (preg_match('/-c(\d+)"/', $line[0], $matches)) {
+				$category_id = $matches[1];
+				
+				$path = array($line[1], $line[2], $line[3], $line[4]);
+				$path = array_map('trim', $path);
+				$path = array_filter($path);
+				$path = implode(' > ' , $path);
+
+				if (!empty($path)) {
+					$save_item = array(
+						'comparator_id' => $comparator_id,
+						'category_id' => $category_id,
+						'path' => $path
+					);
+					
+					$db_categories_comparator = $this->CategoriesComparator->find('first', array(
+						'conditions' => array(
+							'CategoriesComparator.comparator_id' => $comparator_id,
+							'CategoriesComparator.category_id' => $category_id
+						),
+						'contain' => array(),
+						'fields' => array('CategoriesComparator.id')
+					));
+					if (!empty($db_categories_comparator)) {
+						$save_item['id'] = $db_categories_comparator['CategoriesComparator']['id'];
+					}
+					$save[] = $save_item;
+				}
+			}
+		}
+		
+		if (!$this->CategoriesComparator->saveAll($save)) {
+			debug($save);
+		}
+		die('OK');
+	}
 }
