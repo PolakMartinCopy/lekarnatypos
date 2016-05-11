d03bc18f2483e04f43147b8ac5fb777a9a34e367
Martin Polak <brko11@gmail.com>
2016-05-11 17:04:56 +0200

    simplexml_load_file workaround

--- app/controllers/zasilkovna_branches_controller.php
+++ app/controllers/zasilkovna_branches_controller.php
@@ -3,41 +3,45 @@ class ZasilkovnaBranchesController extends AppController {
 	var $name = 'ZasilkovnaBranches';
 	
 	function load() {
-		$xml = simplexml_load_file($this->ZasilkovnaBranch->xmlFile);
-		if ($xml != FALSE) {
-			foreach ($xml->children() AS $child) {
-				$elementName = $child->getName();
-				if ($elementName == 'branches') {
-					$save = array();
-					foreach ($child->children() as $xmlBranch) {
-						$country = $xmlBranch->country->__toString();
-						// preskakuju NEceske pobocky
-						if ($country == 'cz') {
-							$branch = array(
-								'zasilkovna_id' => $xmlBranch->id->__toString(),
-								'name' => $xmlBranch->name->__toString(),
-								'name_street' => $xmlBranch->nameStreet->__toString(),
-								'place' => $xmlBranch->place->__toString(),
-								'street' => $xmlBranch->street->__toString(),
-								'city' => $xmlBranch->city->__toString(),
-								'zip' => $xmlBranch->zip->__toString()
-							);
-							
-							if ($dbBranchId = $this->ZasilkovnaBranch->getIdByField('zasilkovna_id', $branch['zasilkovna_id'])) {
-								$branch['id'] = $dbBranchId;
+		if ($xmlContent = download_url($this->ZasilkovnaBranch->xmlFile)) {
+			$xml = simplexml_load_string($xmlContent);
+			if ($xml != FALSE) {
+				foreach ($xml->children() AS $child) {
+					$elementName = $child->getName();
+					if ($elementName == 'branches') {
+						$save = array();
+						foreach ($child->children() as $xmlBranch) {
+							$country = $xmlBranch->country->__toString();
+							$branch = array();
+							// preskakuju NEceske pobocky
+							if ($country == 'cz') {
+								$branch = array(
+									'zasilkovna_id' => $xmlBranch->id->__toString(),
+									'name' => $xmlBranch->name->__toString(),
+									'name_street' => $xmlBranch->nameStreet->__toString(),
+									'place' => $xmlBranch->place->__toString(),
+									'street' => $xmlBranch->street->__toString(),
+									'city' => $xmlBranch->city->__toString(),
+									'zip' => $xmlBranch->zip->__toString()
+								);
+								if ($dbBranchId = $this->ZasilkovnaBranch->getIdByField($branch['zasilkovna_id'], 'zasilkovna_id')) {
+									$branch['id'] = $dbBranchId;
+								}
+								$save[] = $branch;
 							}
-							$save[] = $branch;
 						}
-					}
-					if (!$this->ZasilkovnaBranch->saveAll($save)) {
-						debug($save);
-						trigger_error('Nepodarilo se ulozit pobocky', E_USER_NOTICE);
+						if (!$this->ZasilkovnaBranch->saveAll($save)) {
+							debug($save);
+							trigger_error('Nepodarilo se ulozit pobocky', E_USER_NOTICE);
+						}
 					}
 				}
 			}
-		}
-		else {
-			echo "DB se nepodařilo naimportovat! Chybí vstupní XML formát.";
+			else {
+				echo "DB se nepodařilo naimportovat! Chybí vstupní XML formát.";
+			}
+		} else {
+			echo "DB se nepodařilo naimportovat! Nepodařilo se stáhnout XML z dané URL.";
 		}
 		die('here');
 	}
--- app/models/zasilkovna_branch.php
+++ app/models/zasilkovna_branch.php
@@ -5,6 +5,4 @@ class ZasilkovnaBranch extends AppModel {
 	var $actsAs = array('Containable');
 
 	var $xmlFile = 'http://www.zasilkovna.cz/api/v3/8d343c98d33c0917/branch.xml';
-//	var $xmlFile = 'http://localhost/files/zasilkovna_branches.xml';
-
 }
